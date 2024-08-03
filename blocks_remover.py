e62f53bb8e555d1d9250d3da86bebd5aebbfee3d
Tomek Grodecki <tgrodecki89@gmail.com>
2024-08-03 15:47:45 +0200

    dodanie drugiej funkcji usuwajacej bloki

--- blocks_remover.py
+++ blocks_remover.py
@@ -3,13 +3,13 @@ from datetime import datetime
 import json
 import time
 
-def remove_blocks_in_time_range(input_folder, delete_start_time, delete_end_time, progress_callback=None):
+current_directory = os.path.dirname(__file__)
 
-    start_time = time.time()
-
+def remove_blocks_in_time_range(input_folder, delete_start_time, delete_end_time, progress_callback=None, check_interrupt=None):
     json_files = [file for file in os.listdir(input_folder) if file.endswith(".json")]
 
     for json_file in json_files:
+        print('asd')
         with open(os.path.join(input_folder, json_file), 'r+') as file:
             block_data = json.load(file)
             block_timestamp = int(block_data["timestamp"])
@@ -19,7 +19,20 @@ def remove_blocks_in_time_range(input_folder, delete_start_time, delete_end_time
                 file.close()
                 os.remove(os.path.join(input_folder, json_file))
                 print(f"Removed: {json_file}")
+                
 
-    end_time = time.time()
-    total_execution_time = end_time - start_time
-    print(f"Całkowity czas wykonania: {total_execution_time} sekundy")
+def remove_blocks_in_range(first_block, last_block):
+        input_folder =  os.path.join(current_directory, "blocks_data")  
+        start_time = time.time()
+        for block_num in range(first_block, last_block + 1):
+            print(f'deleted - {block_num}')
+            block_file_name = f"block_{block_num}.json"
+            block_file_path = os.path.join(input_folder, block_file_name)
+            
+            if os.path.exists(block_file_path):
+                os.remove(block_file_path)
+                print(f"Usunięto: {block_file_name}")
+            else:
+                print(f"Plik nie istnieje: {block_file_name}")
+        end_time = time.time()
+        total_execution_time = end_time - start_time
