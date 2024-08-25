e4ce4253309bd687eec78c648ce53d1dff04c12f
Tomek Grodecki <tgrodecki89@gmail.com>
2024-08-25 21:18:29 +0200

    Create unit tests for 'BlockProcessor' class - Covered edge cases like invalid block number, timestamp, transactions - Ensured proper exception handling and response to interrupts

---
+++ tests/tests_blocks_download/test_block_processor.py
@@ -0,0 +1,92 @@
+import pytest
+from unittest.mock import MagicMock, patch
+from blocks_download import BlockProcessor  
+
+@pytest.fixture
+def setup_block_processor():    
+    mock_api = MagicMock()
+    mock_file_manager = MagicMock()
+    mock_config = MagicMock()
+    mock_config.REQUEST_DELAY = 2          
+    
+    block_processor = BlockProcessor(mock_api, mock_file_manager, mock_config)
+    return block_processor, mock_api, mock_file_manager
+
+class TestBlocksProcessor:
+    # process_block tests #
+    def test_process_block_success(self, setup_block_processor):
+        block_processor, mock_api, mock_file_manager = setup_block_processor        
+        
+        mock_api.get_block_timestamp.return_value = 1723401311
+        mock_api.get_block_transactions.return_value = [
+            {"hash": "0xecf6efe5f8cd86fc7ea9bf9dd4e1617e65172c36d0e1ff8a30149f5339333dec"},
+            {"hash": "0xc281d2fa303eb7a975f2e790e02be5d22d47459685486d55c8ecbd6063740e8a"},
+            {"hash": "0x5c63a10656b11a94fb44762f53e834747529823dcd2a06425397ff95de0622c8"}        ]
+        
+        fetched_block_numbers = []        
+        
+        with patch('time.sleep', return_value=None) as mock_sleep:
+            block_number, result = block_processor.process_block(20507193, [], fetched_block_numbers, None)            
+            
+            assert block_number == 20507193
+            assert result == 1
+            assert fetched_block_numbers == [20507193]
+
+            mock_file_manager.save_to_json.assert_called_once_with({
+                "block_number": 20507193,
+                "timestamp": 1723401311,
+                "transactions": [        
+                    {"hash": "0xecf6efe5f8cd86fc7ea9bf9dd4e1617e65172c36d0e1ff8a30149f5339333dec"},
+                    {"hash": "0xc281d2fa303eb7a975f2e790e02be5d22d47459685486d55c8ecbd6063740e8a"},
+                    {"hash": "0x5c63a10656b11a94fb44762f53e834747529823dcd2a06425397ff95de0622c8"}
+                ]
+            }, "block_20507193.json")
+            mock_sleep.assert_called_once_with(2)
+
+    def test_process_block_already_fetched(self, setup_block_processor):
+        block_processor, mock_api, mock_file_manager = setup_block_processor
+        fetched_block_numbers = [20507193]            
+        block_number, result = block_processor.process_block(20507193, [], fetched_block_numbers, None)        
+
+        assert block_number is None
+        assert result == 1
+
+    def test_process_block_interrupt(self, setup_block_processor):
+        block_processor, mock_api, mock_file_manager = setup_block_processor
+        mock_interrupt_flag = MagicMock()
+        mock_interrupt_flag.value = True            
+        block_number, result = block_processor.process_block(20507193, [], [], mock_interrupt_flag)
+        
+        assert block_number is None
+        assert result == 0
+
+    def test_process_block_exception(self, setup_block_processor):
+        block_processor, mock_api, mock_file_manager = setup_block_processor        
+        mock_api.get_block_timestamp.side_effect = Exception("Test exception")       
+
+        with pytest.raises(Exception, match="Test exception"):
+            block_processor.process_block(20507193, [], [], None)  
+
+    def test_process_block_invalid_block_number(self, setup_block_processor):
+        block_processor, mock_api, mock_file_manager = setup_block_processor    
+
+        with pytest.raises(ValueError, match="Invalid block number"):
+            block_processor.process_block(-1, [], [], None) 
+
+    def test_process_block_invalid_timestamp(self, setup_block_processor):
+        block_processor, mock_api, mock_file_manager = setup_block_processor              
+        mock_api.get_block_timestamp.side_effect = lambda x: -1     
+        
+        with pytest.raises(ValueError, match="Invalid timestamp"):
+            block_processor.process_block(20507193, [], [], None)  
+
+    def test_process_block_invalid_transactions(self, setup_block_processor):
+        block_processor, mock_api, mock_file_manager = setup_block_processor
+        
+        mock_api.get_block_timestamp.return_value = 1723401311
+        mock_api.get_block_transactions.return_value = [
+            {"wrong_key": "0xecf6efe5f8cd86fc7ea9bf9dd4e1617e65172c36d0e1ff8a30149f5339333dec"}  
+        ]
+        
+        with pytest.raises(ValueError, match="Invalid transactions"):
+            block_processor.process_block(20507193, [], [], None)  
