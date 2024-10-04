777c03f40d74644e9d4bb43854d127bd053c48f7
Tomek Grodecki <tgrodecki89@gmail.com>
2024-10-04 18:46:23 +0200

    Refactor code: implement singleton
    - Updated "LoggerConfig" class to follow the singleton design pattern.

--- logger.py
+++ logger.py
@@ -1,29 +1,39 @@
 import logging
 import os
 from config import Config
+import os
 
 class LoggerConfig:
-    def __init__(self, log_file='app.log', log_level=logging.INFO):
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(log_level)
-        self._configure_logger(log_file)
+    _instance = None    
+    _lock = multiprocessing.Lock()
 
-    def _configure_logger(self, log_file):
+    def __new__(cls, log_file='app.log', log_level=logging.DEBUG):
+        with cls._lock:
+            if cls._instance is None:
+                cls._instance = super(LoggerConfig, cls).__new__(cls)
+                cls._instance._initialize(log_file, log_level)
+        return cls._instance
+
+    def _initialize(self, log_file, log_level):        
+        self.logger = logging.getLogger("simple_logger")
+        self.logger.setLevel(log_level)
+        
+        log_format = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+        
         log_directory = "logs"
         if not os.path.exists(log_directory):
             os.makedirs(log_directory)
-
-        log_path = os.path.join(log_directory, log_file)
-
-        log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
+        
+        log_path = os.path.join(log_directory, log_file)        
+        
+        file_handler = logging.FileHandler(log_path, encoding='utf-8')
+        file_handler.setFormatter(log_format)
+        self.logger.addHandler(file_handler)
+        
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(log_format)
         self.logger.addHandler(console_handler)
 
-        file_handler = logging.FileHandler(log_path, encoding='utf-8')
-        file_handler.setFormatter(log_format)
-        self.logger.addHandler(file_handler)
-
     def get_logger(self):
-        return self.logger
+        print("returned")
+        return self.logger 
