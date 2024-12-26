e302e5c3790b2cb72235f3f3c87f68ee718dd832
Tomek Grodecki <tgrodecki89@gmail.com>
2024-12-26 17:35:00 +0100

    Refactor code
    - Added LoggerConfig for centralized logger creation, accessible via 'logger'
    - Removed Lock.
    - Simplified '__new__' method for cleaner code.
    - Updated logger to use variables from global Config module.

--- logger.py
+++ logger.py
@@ -3,28 +3,25 @@ import os
 from config import Config
 import os
 
+
 class LoggerConfig:
     _instance = None    
-    _lock = multiprocessing.Lock()
 
-    def __new__(cls, log_file='app.log', log_level=logging.DEBUG):
-        with cls._lock:
-            if cls._instance is None:
-                cls._instance = super(LoggerConfig, cls).__new__(cls)
-                cls._instance._initialize(log_file, log_level)
+    def __new__(cls):        
+        if cls._instance is None:
+                cls._instance = super().__new__(cls)
+                cls._instance._initialize()
         return cls._instance
 
-    def _initialize(self, log_file, log_level):        
+    def _initialize(self):        
+        config = Config()
+
         self.logger = logging.getLogger("simple_logger")
-        self.logger.setLevel(log_level)
+        self.logger.setLevel(logging.DEBUG)
         
         log_format = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
         
-        log_directory = "logs"
-        if not os.path.exists(log_directory):
-            os.makedirs(log_directory)
-        
-        log_path = os.path.join(log_directory, log_file)        
+        log_path = config.LOG_FILE
         
         file_handler = logging.FileHandler(log_path, encoding='utf-8')
         file_handler.setFormatter(log_format)
@@ -37,3 +34,7 @@ class LoggerConfig:
     def get_logger(self):
         print("returned")
         return self.logger 
+
+
+logger_config = LoggerConfig()
+logger = logger_config.get_logger()
