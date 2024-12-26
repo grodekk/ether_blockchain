bab08367d428ea7493b9dd3054fb059cce12f732
Tomek Grodecki <tgrodecki89@gmail.com>
2024-12-26 23:17:51 +0100

    Refactor code: Add enviromental variables and improve configuration

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     _instance = None
@@ -10,12 +13,21 @@ class Config:
         return cls._instance
 
     def initialize(self):
-        self.CURRENT_DIRECTORY = os.path.dirname(__file__)
-        self.API_KEY = "ZA1WF2Z9ZFJBGWPI8N4C6F6ARUVD3K7K5E"
-        self.API_URL = "https://api.etherscan.io/api"
-        self.REQUEST_DELAY = 0.2
-        self.BLOCKS_DATA_DIR = os.path.join(self.CURRENT_DIRECTORY, 'blocks_data')
-        self.BLOCKS_DATA_FILE = os.path.join(self.CURRENT_DIRECTORY, 'blocks_data.json')
+
+        self.BASE_DIR = os.getenv("APP_BASE_DIR", os.getcwd())
+        self.API_KEY = os.getenv("API_KEY", "")
+        self.API_URL = os.getenv("API_URL", "https://api.etherscan.io/api")
+        self.REQUEST_DELAY = float(os.getenv("REQUEST_DELAY", 0.2))
+
+        self.LOG_DIR = os.path.join(self.BASE_DIR, "logs")
+        self.LOG_FILE = os.path.join(self.LOG_DIR, "app.log")
+        self.DB_FILENAME = os.path.join(self.BASE_DIR, "baza_danych.db")
+
+        self.INTERESTING_INFO_DIR = os.path.join(self.BASE_DIR, "interesting_info")
+        self.WALLETS_ACTIVITY_FILENAME = os.path.join(self.BASE_DIR, 'interesting_info', 'Biggest_wallets_activity.json')
+        self.BLOCKS_DATA_DIR = os.path.join(self.BASE_DIR, "blocks_data")
+        self.BLOCKS_DATA_FILE = os.path.join(self.BASE_DIR, 'blocks_data.json')
+        self.OUTPUT_FILE_PATH = os.path.join(self.BASE_DIR, "interesting_info", "Biggest_wallets_activity.json")
         self.JSON_FILES = [file for file in os.listdir(self.BLOCKS_DATA_DIR) if file.endswith(".json")]
-        self.LOG_DIRECTORY = os.path.join(self.CURRENT_DIRECTORY, 'logs')
-        self.LOG_FILE = os.path.join(self.LOG_DIRECTORY, 'app.log')
+        self.OUTPUT_FOLDER = "interesting_info"
+        self.PROGRESS_DATA_FILE = os.path.join(self.BASE_DIR, "progress.json")
