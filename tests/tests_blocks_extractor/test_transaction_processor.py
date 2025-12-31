6740892d8db8c4cdd8bf94e328d2905313c3a1f1
Tomasz Grodecki <tgrodecki89@gmail.com>
2025-12-31 06:05:36 +0100

    refactored tests

--- tests/tests_blocks_extractor/test_transaction_processor.py
+++ tests/tests_blocks_extractor/test_transaction_processor.py
@@ -29,7 +29,7 @@ class TestTransactionProcessor:
             assert processor.total_fees == 0.000021
             assert processor.total_value_eth == 1.0
         
-    #
+
     def test_process_transaction_missing_key(self, processor, caplog):        
         transaction = {
             "from": "0xSenderAddress",
@@ -40,10 +40,10 @@ class TestTransactionProcessor:
 
         with caplog.at_level('ERROR'):
             with pytest.raises(CustomProcessingError) as exc_info:
-                processor.process_transaction(transaction)            
-            
-            assert "TransactionProcessor.process_transaction - Missing key" in caplog.text
-            assert "KeyError" in str(exc_info.value)
+                processor.process_transaction(transaction)
+
+            assert "KeyError" in caplog.text
+            assert "value" in caplog.text
     
 
     def test_process_transaction_invalid_value(self, processor, caplog):        
@@ -59,43 +59,30 @@ class TestTransactionProcessor:
             with pytest.raises(CustomProcessingError) as exc_info:
                 processor.process_transaction(transaction)            
             
-            assert "TransactionProcessor.process_transaction - Invalid value" in caplog.text
-            assert "ValueError" in str(exc_info.value)
+            assert "invalid_value" in caplog.text
 
 
     def test_process_transaction_type_error(self, processor, caplog):        
         transaction = {
             "from": "0xSenderAddress",
             "to": "0xReceiverAddress",
-            "value": "0x1",
+            "value": None,
             "gasPrice": "0x5",
             "gas": "0x10"
         }        
-        
-        transaction["value"] = None
 
         with caplog.at_level('ERROR'):
-            with pytest.raises(CustomProcessingError) as exc_info:
+            with pytest.raises(CustomProcessingError):
                 processor.process_transaction(transaction)
         
-        assert "TransactionProcessor.process_transaction - Type error" in caplog.text
-        assert "TypeError" in str(exc_info.value)
+        assert "TypeError" in caplog.text
 
 
-    def test_process_transaction_unexpected_error(self, processor, caplog):        
-        processor._summarize = MagicMock(side_effect=Exception("Unexpected error during summarization"))
-
-        transaction = {
-            "from": "0xSenderAddress",
-            "to": "0xReceiverAddress",
-            "value": "0xde0b6b3a7640000",
-            "gasPrice": "0x3b9aca00",
-            "gas": "0x5208"
-        }
-
+    def test_process_transaction_unexpected_error(self, processor, caplog):
+        transaction = MagicMock()
+        transaction.__getitem__.side_effect = Exception("Unexpected error during transaction processing")
         with caplog.at_level("ERROR"):
-            with pytest.raises(CustomProcessingError) as exc_info:
+            with pytest.raises(CustomProcessingError):
                 processor.process_transaction(transaction)            
             
-            assert "TransactionProcessor.process_transaction - Unexpected error : Unexpected error during summarization" in caplog.text
-            assert "General Exception" in str(exc_info.value)
+            assert "Unexpected error during transaction processing" in caplog.text
